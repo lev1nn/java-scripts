9bd6305dd1745e8bd697c3d6b34eb96be19727b2
levin schaller <103995309+lev1nn@users.noreply.github.com>
2022-08-19 14:43:31 +0200

    added precise error messages

--- src/projects/loginsystem/LoginSystem.java
+++ src/projects/loginsystem/LoginSystem.java
@@ -8,29 +8,49 @@ public class LoginSystem {
 
         FileReader db = new FileReader();
 
-        System.out.print("Username: "); // TODO let user create new acc if he hasn't one
+        System.out.print("username: "); // TODO let user create new acc if he hasn't one
         String user = scanner.nextLine();
-        System.out.println(); // TODO check if user exists
-        System.out.print("Password: ");
+        System.out.println();
+        validUser(user, db);
+
+        System.out.print("password: ");
         String password = scanner.nextLine();
         System.out.println();
 
         if (validPassword(user, password, db)) {
             System.out.println("success, you are logged in ");
-        } else {
-            System.out.println("failed to log in, please try again "); // TODO more accurate error messages
+        }
+    }
+
+    private static void validUser(String user, FileReader db) {
+        if (user == null || user.equals("") || user.matches("[ \t]+")) {
+            System.out.println("empty user, please try again");
+            System.exit(0);
+        } else if (!db.containsUser(user)) {
+            System.out.println("user does not exist, please try again ");
+            System.exit(0);
         }
     }
 
     public static boolean validPassword(String user, String password, FileReader db) {
         String pwFromDB = db.getPasswordForUser(user);
 
-        if (pwFromDB != null) {
-            return pwFromDB.equals(password);
+        if (pwFromDB == null) {
+            System.out.println("failed to log in [problem in database] please try again ");
+            return false;
+        } else if (password == null || password.equals("") || password.matches("[ \t]+")) {
+            System.out.println("failed to log in [empty password] please try again ");
+            return false;
+        } else if (pwFromDB.equals(password)) {
+            return true;
+        } else if (!pwFromDB.equals(password)) {
+            System.out.println("failed to log in [invalid password] please try again ");
+            return false;
         } else {
+            System.out.println("failed to log in [unknown error] please try again ");
             return false;
         }
-
-        // return password.equals(pwFromDB);
     }
 }
+
+
