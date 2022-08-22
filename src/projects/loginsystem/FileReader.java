4ce17d1ae41d708d02642d4e0d0af9354a83eb32
levin schaller <103995309+lev1nn@users.noreply.github.com>
2022-08-22 15:58:09 +0200

    added register function

--- src/projects/loginsystem/FileReader.java
+++ src/projects/loginsystem/FileReader.java
@@ -1,6 +1,6 @@
 package projects.loginsystem;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 
 public class FileReader {
     private Map<String, String> userDB = new HashMap<>(); // creates hashmap as "database"
-    private final String FILE_PATH = "C:\\Users\\Levin\\OneDrive\\Desktop\\Levin\\Programmieren\\Privat\\src\\projects\\loginsystem\\Users.txt"; // path textfile with usernames and passwords
+    private static final String FILE_PATH = "C:\Users\Levin\OneDrive\Desktop\Levin\Programmieren\Privat\src\projects\loginsystem\Users.txt"; // path textfile with usernames and passwords
 
     public FileReader() {
         try (Stream<String> lines = Files.lines(Paths.get(FILE_PATH))) { // reads textfile and writes into hashmap
@@ -20,9 +20,19 @@ public class FileReader {
                         String value = keyValuePair[1]; // string2 -> value -> password
                         userDB.put(key, value); // -> puts both strings into hashmap
                     });
-        } catch (IOException e) { // exeption 
+        } catch (IOException e) { // exception
             e.printStackTrace();
         }
+    }
+
+    public static void FileWriter(String newUser) throws IOException {
+        FileWriter fileWriter = new FileWriter(FILE_PATH, true); // creates new fileWriter
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write("\n" + newUser); // creates new user in textfile db users
+        bufferedWriter.close();
+
+        System.out.println("user has been created, please restart and try to log in "); // success message
+        System.exit(0);
     }
 
     public boolean containsUser(String user) { // checks if the entered user is in the db
@@ -30,7 +40,7 @@ public class FileReader {
     }
 
     public String getPasswordForUser(String user) { // checks if the entered password is correct
-        if (containsUser(user)) {
+        if (containsUser(user)) { 
             return userDB.get(user);
         } else {
             return null;
--- src/projects/loginsystem/LoginSystem.java
+++ src/projects/loginsystem/LoginSystem.java
@@ -1,5 +1,6 @@
 package projects.loginsystem;
 
+import java.io.IOException;
 import java.util.*;
 
 public class LoginSystem {
@@ -9,17 +10,17 @@ public class LoginSystem {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        FileReader db = new FileReader(); // instance from FileReader
-        String user;
+        FileReader db = new FileReader(); // instance from filereader
+        String user; // some declarations
         String password;
-        int badPasswordCounter = 0;
-        int maxBadPasswords = 10;
+        int badPasswordCounter = 0; // initialize badpasswordcounter
+        int maxBadPasswords = 10; // initialize maxbadpasswords
 
         do {
-            System.out.print("username: "); // TODO let user create new acc if he hasn't one
+            System.out.print("username: ");
             user = scanner.nextLine(); // input -> user
             System.out.println();
-        } while (!validUser(user, db)); // checks if username is valid, else it repeats the step
+        } while (!validUser(user, db, scanner)); // checks if username is valid, else it repeats the step or user can create a new account
 
         do {
             System.out.print("password: ");
@@ -31,21 +32,28 @@ public class LoginSystem {
                 break;
             }
             badPasswordCounter++; // otherwise, it sets the counter one higher
-        } while (badPasswordCounter < maxBadPasswords);
+        } while (badPasswordCounter < maxBadPasswords); // while max of bad password isn't reached, it makes the pw query
 
-        if (badPasswordCounter >= maxBadPasswords) { // if the counter is to high, it prints a error message
+        if (badPasswordCounter >= maxBadPasswords) { // if the counter is to high, it prints an error message
             System.out.println("failed to log in " + ANSI_RED + "[badPasswordCounter to high]" + ANSI_RESET + " please try again later or contact the administrator ");
             System.exit(0);
         }
     }
 
-    private static boolean validUser(String user, FileReader db) {
+    private static boolean validUser(String user, FileReader db, Scanner scanner) {
         if (user == null || user.equals("") || user.matches("[ \t]+")) { // checks if user is empty
             System.out.println("error " + ANSI_RED + "[empty user]" + ANSI_RESET + " please try again"); // -> error message
             System.out.println();
             return false;
         } else if (!db.containsUser(user)) { // checks if user isn't in db
-            System.out.println("error " + ANSI_RED + "[user doesn't exist]" + ANSI_RESET + " please try again"); // -> error message
+            System.out.println("error " + ANSI_RED + "[user doesn't exist]" + ANSI_RESET + " please try again or type 'new' for create a new user"); // -> error message, asks if user wants to create a new user
+            if (scanner.nextLine().equals("new")) {
+                try {
+                    createUser(user, db, scanner);
+                } catch (IOException e) { // exception
+                    e.printStackTrace();
+                }
+            }
             System.out.println();
             return false;
         } else {
@@ -76,6 +84,12 @@ public class LoginSystem {
             return false;
         }
     }
+
+    private static void createUser(String user, FileReader db, Scanner scanner) throws IOException { // creates new user if user wants to create a new account
+        System.out.println();
+        System.out.printf("password for user %s: ", user);
+        String newUser = (user + "," + scanner.nextLine()); // creates string in format [user,password]
+        System.out.println();
+        FileReader.FileWriter(newUser); // method to creates new user in db
+    }
 }
-
-
