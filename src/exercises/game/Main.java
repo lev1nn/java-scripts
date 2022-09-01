819a32d7fe6658e27e55b5f1fe0158c2f5322f1f
levin schaller <103995309+lev1nn@users.noreply.github.com>
2022-09-01 15:49:10 +0200

    updated game

--- src/exercises/game/Main.java
+++ src/exercises/game/Main.java
@@ -6,16 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        /*System.out.println("name from player1 ");
+        System.out.println("name from player1 ");
         String name1 = scanner.nextLine();
         System.out.println("");
 
         System.out.println("name from player2 ");
         String name2 = scanner.nextLine();
-        System.out.println("");*/
-
-        String name1 = "robin";
-        String name2 = "levin";
+        System.out.println("");
 
         Character player1 = new Character(name1, 100, 25);
         Character player2 = new Character(name2, 100, 25);
